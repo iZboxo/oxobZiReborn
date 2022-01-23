71a12de92b08b46dc4afc4fe9479b1a5342b2ae0
iZboxo <zdenda.havlat@gmail.com>
2022-01-23 17:43:50 +0100

    oxobZi-1.0.0

---
+++ commands/evropa2.js
@@ -0,0 +1,29 @@
+const Discord = require('discord.js')
+const { Command, CommandType } = require('gcommands');
+
+// Create a new command with the name 'hello'
+new Command({
+	name: 'evropa2',
+	description: 'Pustí rádio!',
+	// GCommands Next offers different types of commands, we will only use slash and message commands here.
+	type: [CommandType.SLASH, CommandType.MESSAGE],
+	// The function thats executed when the user uses the command.
+	run: (message) => {
+		if (message.member.voice.channel) {
+            message.member.voice.channel.join()
+              .then(connection => {
+                const embedik = new Discord.MessageEmbed()
+                .setColor("RANDOM")
+                .setTitle("Evropa 2")
+                .setImage("https://img.x-function.tech/ev2.jpg")
+                .setFooter("Best radio")
+                .setTimestamp();
+                  message.reply(embedik)
+                  connection.play(`http://ice.actve.net/fm-evropa2-128`);
+              })
+              .catch(console.log);
+          } else {
+            message.reply(':x: You must be in channel!');
+          }
+	}
+});
