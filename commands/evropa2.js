294c1707923e484e57d451e87110e4530a78fa5c
iZboxo <zdenda.havlat@gmail.com>
2022-01-23 17:52:50 +0100

    'oxobZi-1.0.1-Updates'

--- commands/evropa2.js
+++ commands/evropa2.js
@@ -10,23 +10,22 @@ new Command({
 	// The function thats executed when the user uses the command.
 	run: async(message) => {
 		if (message.member.voice) {
-            const channel = await message.client.channels.fetch(message.member.voice.channelId)
-            const connection = await channel.join();
+      const channel = await message.client.channels.fetch(message.member.voice.channelId)
+      const connection = await channel.join();
 
-            c
-              /*.then(connection => {
-                const embedik = new Discord.MessageEmbed()
-                .setColor("RANDOM")
-                .setTitle("Evropa 2")
-                .setImage("https://img.x-function.tech/ev2.jpg")
-                .setFooter("Best radio")
-                .setTimestamp();
-                  message.reply(embedik)
-                  connection.play(`http://ice.actve.net/fm-evropa2-128`);
-              })*/
-              .catch(console.log);
-          } else {
-            message.reply(':x: You must be in channel!');
-          }
+      const stream = await got.stream('http://ice.actve.net/fm-evropa2-128');
+
+      connection.play(stream);
+
+      const embedik = new Discord.MessageEmbed()
+        .setColor("RANDOM")
+        .setTitle("Evropa 2")
+        .setImage("https://img.x-function.tech/ev2.jpg")
+        .setFooter("Best radio")
+        .setTimestamp();
+      message.reply(embedik)
+    } else {
+      message.reply(':x: You must be in channel!');
+    }
 	}
 });
