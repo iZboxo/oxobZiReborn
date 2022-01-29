34929b36a05f231237d3cf6032be4341e010e057
iZboxo <zdenda.havlat@gmail.com>
2022-01-29 20:31:04 +0100

    'oxobZi-1.0.3-Updates'

--- commands/radio.js
+++ commands/radio.js
@@ -17,7 +17,7 @@ new Command({
       RadioPlayer.startRadio(message.guild, message.member.voice.channel);
 
       const res = (await hyttpo.get(`http://${process.env.ip}/api/nowplaying/oxoRaNCS`)).data;
-      console.log(res.station.now_playing);
+      console.log(res);
 
       const embedik = new MessageEmbed()
         .setColor("RANDOM")
