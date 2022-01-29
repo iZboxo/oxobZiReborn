d1112028d00eb683cb535be5f72e953e47422e55
iZboxo <zdenda.havlat@gmail.com>
2022-01-29 20:54:14 +0100

    'oxobZi-1.0.3-Updates'

--- commands/radio.js
+++ commands/radio.js
@@ -22,7 +22,7 @@ new Command({
         .setColor("RANDOM")
         .setTitle("Started playing oxoRaNCS!")
         //.setImage("https://img.x-function.tech/ev2.jpg")
-        .setDescription(`Now playing: ${res.now_playing.song.text}`)
+        .setDescription(`Now playing: **${res.now_playing.song.text}**`)
         .setFooter({ text: '- iZboxo.cz' })
         .setTimestamp();
       message.reply({ embeds: [ embedik ] });
