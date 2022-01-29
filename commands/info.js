7276d5da42ca2470eb14226b7749fe471a05bb6b
iZboxo <zdenda.havlat@gmail.com>
2022-01-29 22:26:46 +0100

    'oxobZi-1.0.3-Updates'

--- commands/info.js
+++ commands/info.js
@@ -1,24 +1,13 @@
 const { Command, CommandType } = require('gcommands');
-const { MessageEmbed } = require('discord.js')
-const got = require('got');
-const hyttpo = require('hyttpo').default;
 
+// Create a new command with the name 'hello'
 new Command({
 	name: 'info',
-	description: 'Get bot info!',
+	description: 'get info!',
+	// GCommands Next offers different types of commands, we will only use slash and message commands here.
 	type: [CommandType.SLASH, CommandType.MESSAGE],
-	run: (message) => {
-
-        //const endemit = (await hyttpo.get(`https://vicky.izboxo.cz/image`)).data;
-        //console.log(endemit.image);
-
-		const embedik = new MessageEmbed()
-        .setColor("RANDOM")
-        .setTitle("Started playing oxoRaNCS!")
-        //.setImage("https://img.x-function.tech/ev2.jpg")
-        //.setDescription(`Now playing: **${output.now_playing.song.text}**`)
-        .setFooter({ text: '- iZboxo.cz' })
-        .setTimestamp();
-      message.reply({ embeds: [ embedik ] });
+	// The function thats executed when the user uses the command.
+	run: async(message) => {
+		return message.reply(`Hello ${ctx.user.username}!`);
 	}
 });
