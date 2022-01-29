1f6b2c93437832a0b1e26c23f314a0eaa0c63a50
iZboxo <zdenda.havlat@gmail.com>
2022-01-29 22:27:57 +0100

    'oxobZi-1.0.3-Updates'

--- commands/info.js
+++ commands/info.js
@@ -1,12 +1,11 @@
 const { Command, CommandType } = require('gcommands');
+const hyttpo = require('hyttpo').default;
 
-// Create a new command with the name 'hello'
+
 new Command({
 	name: 'info',
 	description: 'get info!',
-	// GCommands Next offers different types of commands, we will only use slash and message commands here.
 	type: [CommandType.SLASH, CommandType.MESSAGE],
-	// The function thats executed when the user uses the command.
 	run: async(message) => {
 		return message.reply(`Hello ${message.user.username}!`);
 	}
