e69dc45d6cafcf8d0b59527a9fe4886fe61aecec
iZboxo <zdenda.havlat@gmail.com>
2022-01-29 22:11:28 +0100

    'oxobZi-1.0.3-Updates'

---
+++ commands/info.js
@@ -0,0 +1,19 @@
+const { Command, CommandType } = require('gcommands');
+const { MessageEmbed } = require('discord.js')
+const got = require('got');
+
+new Command({
+	name: 'hello',
+	description: 'Says hello!',
+	type: [CommandType.SLASH, CommandType.MESSAGE],
+	run: (message) => {
+		const embedik = new MessageEmbed()
+        .setColor("RANDOM")
+        .setTitle("Started playing oxoRaNCS!")
+        //.setImage("https://img.x-function.tech/ev2.jpg")
+        .setDescription(`Now playing: **${output.now_playing.song.text}**`)
+        .setFooter({ text: '- iZboxo.cz' })
+        .setTimestamp();
+      message.reply({ embeds: [ embedik ] });
+	}
+});
