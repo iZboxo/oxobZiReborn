0786ecd021f4911b067dd87502e419b0a3f1a918
iZboxo <zdenda.havlat@gmail.com>
2022-01-30 13:29:29 +0100

    'oxobZi-1.0.3-Updates'

--- commands/radio.js
+++ commands/radio.js
@@ -5,13 +5,10 @@ const RadioPlayer = require('../structures/RadioPlayer');
 const { Command, CommandType } = require('gcommands');
 const hyttpo = require('hyttpo').default;
 
-// Create a new command with the name 'hello'
 new Command({
 	name: 'radio',
 	description: 'Listen to music!',
-	// GCommands Next offers different types of commands, we will only use slash and message commands here.
 	type: [CommandType.SLASH, CommandType.MESSAGE],
-	// The function thats executed when the user uses the command.
 	run: async(message) => {
 		if (message.member.voice.channel) {
       RadioPlayer.startRadio(message.guild, message.member.voice.channel);
