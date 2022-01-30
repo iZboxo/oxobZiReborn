b7918024018a56af87bcc823e04e4bcb7efe552c
iZboxo <zdenda.havlat@gmail.com>
2022-01-30 13:55:46 +0100

    'oxobZi-1.0.3-Updates'

--- commands/radio.js
+++ commands/radio.js
@@ -2,19 +2,40 @@ const { MessageEmbed } = require('discord.js')
 const got = require('got');
 const { AudioPlayerStatus } = require("@discordjs/voice");
 const RadioPlayer = require('../structures/RadioPlayer');
-const { Command, CommandType } = require('gcommands');
+const { Command, CommandType, ArgumentType } = require('gcommands');
 const hyttpo = require('hyttpo').default;
 
 new Command({
-	name: 'radio',
+	name: 'radioo',
 	description: 'Listen to music!',
 	type: [CommandType.SLASH, CommandType.MESSAGE],
+  arguments: [
+    {
+        name: 'nowplaying',
+        type: ArgumentType.SUB_COMMAND,
+        description: 'Show what is plaing right now!'
+    }/*,
+    {
+        name: 'ping',
+        type: ArgumentType.SUB_COMMAND,
+        description: 'Ping'
+    }*/
+],
 	run: async(message) => {
+
+        const output = (await hyttpo.get(`http://${process.env.ip}/api/nowplaying/oxoRaNCS`)).data;
+      const output2 = (await hyttpo.get(`https://vicky.izboxo.cz/image`)).data;
+
+        const subCommand = message.arguments.getSubcommand();
+
+        if (subCommand === 'nowplaying') {
+            return message.reply(`Now playing: **${output.now_playing.song.text}** !`);
+        } /*else if(subCommand === 'ping') {
+            return message.reply(`Ping pong bot is working fine! ${message.user.username}!`);
+        }*/
+
 		if (message.member.voice.channel) {
       RadioPlayer.startRadio(message.guild, message.member.voice.channel);
-
-      const output = (await hyttpo.get(`http://${process.env.ip}/api/nowplaying/oxoRaNCS`)).data;
-      const output2 = (await hyttpo.get(`https://vicky.izboxo.cz/image`)).data;
 
       const embedik = new MessageEmbed()
         .setColor("RANDOM")
