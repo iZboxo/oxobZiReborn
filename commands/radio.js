59020ac880433b578000ac342470fd997ce3400d
iZboxo <zdenda.havlat@gmail.com>
2022-01-30 15:25:22 +0100

    'oxobZi-1.0.3-Updates'

--- commands/radio.js
+++ commands/radio.js
@@ -30,7 +30,8 @@ new Command({
 
         if (subCommand === 'nowplaying') {
             return message.reply(`Now playing: **${output.now_playing.song.text}** !`);
-        } else if(subCommand === 'play') {
+        }
+        else if(subCommand === 'play') {
           if (message.member.voice.channel) {
             RadioPlayer.startRadio(message.guild, message.member.voice.channel);
       
