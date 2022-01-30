ce6d9fdbfaf1f63e5dcb593cebcc44a0b9485e55
iZboxo <zdenda.havlat@gmail.com>
2022-01-30 10:47:32 +0100

    'oxobZi-1.0.3-Updates'

--- commands/ping.js
+++ commands/ping.js
@@ -2,12 +2,12 @@ const { Command, CommandType } = require('gcommands');
 
 // Create a new command with the name 'hello'
 new Command({
-	name: 'hello',
-	description: 'Says hello!',
+	name: 'ping',
+	description: 'What this should do?',
 	// GCommands Next offers different types of commands, we will only use slash and message commands here.
 	type: [CommandType.SLASH, CommandType.MESSAGE],
 	// The function thats executed when the user uses the command.
 	run: (ctx) => {
-		return ctx.reply(`Hello ${ctx.user.username}!`);
+		return ctx.reply(`Ping pong bot is working fine! ${ctx.user.username}!`);
 	}
 });
