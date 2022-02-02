61fd705ba70f04ac91420227d9ec0e0ea963f8d3
iZboxo <zdenda.havlat@gmail.com>
2022-02-02 17:18:13 +0100

    'oxobZi-1.0.4-Updates'

--- commands/botinfo.js
+++ commands/botinfo.js
@@ -1,17 +1,13 @@
 const { Command, CommandType } = require('gcommands');
 const hyttpo = require('hyttpo').default;
-const Discord = require('discord.js')
-const moment = require('moment');
-const si = require('systeminformation');
-const momentDurationFormatSetup = require("moment-duration-format");
-momentDurationFormatSetup(moment);
-const delay = require("delay");
 
 new Command({
-	name: 'botinfo',
-	description: 'Get bot botinfo!',
+	name: 'info',
+	description: 'Get bot info!',
 	type: [CommandType.SLASH, CommandType.MESSAGE],
 	run: async(message) => {
-        return message.reply(`${message.ws.ping}`);
+		const endemit = (await hyttpo.get(`https://vicky.izboxo.cz/image`)).data;
+
+        return message.reply(endemit.image);
 	}
 });
