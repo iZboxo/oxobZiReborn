351662355305e317e6f29f61f47a406107860640
iZboxo <zdenda.havlat@gmail.com>
2022-02-04 22:21:28 +0100

    'oxobZi-1.0.6-Updates'

--- index.js
+++ index.js
@@ -4,12 +4,10 @@ const { setupPlayer } = require('./structures/RadioPlayer');
 const { Intents, Collection } = require('discord.js');
 const { join } = require('path');
 
-// Set the default cooldown for commands
 Command.setDefaults({
 	cooldown: '5s',
 });
 
-// Set the default onError function for components
 Component.setDefaults({
 	onError: (ctx, error) => {
 		return ctx.reply('Oops! Something went wrong')
@@ -17,17 +15,13 @@ Component.setDefaults({
 });
 
 
-// Search for plugins in node_modules (folder names starting with gcommands-plugin-) or plugins folder
 Plugins.search(__dirname);
 
 const client = new GClient({
-	// Register the directories where your commands/components/listeners will be located.
 	dirs: [
 		join(__dirname, 'commands')
 	],
-	// Set the prefix for message commands
 	messagePrefix: '!',
-	// Set the guild where you will be developing your bot. This is usefull cause guild slash commands update instantly.
 	devGuildId: process.env.DEV_SERVER,
 	// Set the intents you will be using (https://discordjs.guide/popular-topics/intents.html#gateway-intents)
 	intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.GUILD_VOICE_STATES],
