530648a8cb305bff5d83fb19234a12b273be646f
iZboxo <zdenda.havlat@gmail.com>
2022-02-18 13:08:25 +0100

    'oxobZi-1.0.7-Updates'

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ Command.setDefaults({
 
 Component.setDefaults({
 	onError: (ctx, error) => {
-		return ctx.reply('Error! Please contact developer! Cézar')
+		return ctx.reply('Error! Please contact developer! Cézar S')
 	} 
 });
 
