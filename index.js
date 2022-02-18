f277f2a751f10b703a922f68a265e5a745bbf565
iZboxo <zdenda.havlat@gmail.com>
2022-02-18 13:08:32 +0100

    'oxobZi-1.0.7-Updates'

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ Command.setDefaults({
 
 Component.setDefaults({
 	onError: (ctx, error) => {
-		return ctx.reply('Error! Please contact developer! Cézar Salát')
+		return ctx.reply('Error! Please contact developer! Cézar Salát#')
 	} 
 });
 
